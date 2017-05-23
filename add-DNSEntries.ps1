39e2cbb44cf9a5bb11676a830e7d387bebcce83d
noendscripting <miresnic@microsoft.com>
2017-05-23 11:43:10 -0400

    add new file update get-dns

---
+++ add-DNSEntries.ps1
@@ -0,0 +1,97 @@
+﻿<#
+.SYNOPSIS  
+  The script add DNS entries into sepcified DNS Zone from a provide csv file
+.DESCRIPTION
+  Script adds DNS entries other than of SOA and NS type from formatted csv file.
+  Warning: this script uses WMI and might cause perfomance problems on servers   with large number of DNS records and older hardware.
+  DISCLAIMER
+    THIS SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED,
+    INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.  
+    We grant You a nonexclusive, royalty-free right to use and modify the Sample Code and to reproduce and distribute the object
+    code form of the Sample Code, provided that You agree: (i) to not use Our name, logo, or trademarks to market Your software
+    product in which the Sample Code is embedded; (ii) to include a valid copyright notice on Your software product in which the
+    Sample Code is embedded; and (iii) to indemnify, hold harmless, and defend Us and Our suppliers from and against any claims
+    or lawsuits, including attorneys’ fees, that arise or result from the use or distribution of the Sample Code.
+    Please note: None of the conditions outlined in the disclaimer above will supersede the terms and conditions contained within
+    the Premier Customer Services Description.
+ .PARAMETER dnsZone
+  Name of the DNS zone from where records are collected
+ .PARAMETER DNSServer
+  Name of the DNS server where the DNS zone is hosted. Can use "." for local server
+ .PARAMETER csvFile
+  Path to the CSV file with DNS record information
+  
+  .EXAMPLE
+  ./get-DNSEntries.ps1 -dnsZone contosoad.com -DNSServer dns1.contoso.com -csvFile c:\dnsentries.csv
+   
+#>
+
+[CmdletBinding()]
+
+param(
+[Parameter(Mandatory=$true)]
+[string]$csvFile,
+[Parameter(Mandatory=$true)]
+[string]$DNSserver,
+[Parameter(Mandatory=$true)]
+[string]$dnsZone
+)
+
+$records = Import-Csv -Path $csvFile
+
+
+$CurrentDNSconfig = Get-WMIObject -ComputerName $DNSserver  -Namespace 'root\MicrosoftDNS' -Class MicrosoftDNS_ResourceRecord |? {$_.ContainerName -eq $dnsZone}
+if($CurrentDNSconfig -eq $null)
+{
+
+ Write-Host "$($dnsZone) DNS Zone not found on server $($DNSserver) please create zone and re-run the script"
+ exit
+
+
+}
+
+
+ForEach ($srcRec in $Records) {            
+            
+    # Echo the source record data for logging            
+    $srcRec            
+            
+    $class         = $srcRec.class          # A, CNAME, PTR, etc.            
+    $ownerName     = $srcRec.OwnerName        # Name column in DNS GUI, FQDN            
+    $containerName = $srcRec.ContainerName    # Zone FQDN            
+    $domainName    = $srcRec.DomainName       # Zone FQDN            
+    $ttl           = $srcRec.TTL              # TTL            
+    $recordClass   = $srcRec.RecordClass      # Usually 1 (IN)            
+    $recordData    = $srcRec.RecordData       # Data column in DNS GUI, value            
+            
+    # Dynamically create a new record of the appropriate type (class)            
+    $destRec = [WmiClass]"\\$dnsServer\root\MicrosoftDNS:$class"            
+                
+    # The CreateInstanceFromPropertyData method varies slightly based on the            
+    # record type (class).            
+    Switch ($class) {            
+        MicrosoftDNS_AType {            
+            $destRec.CreateInstanceFromPropertyData($destServer, $dnsZone,$ownerName, $recordClass, $ttl, $recordData)            
+        }            
+        MicrosoftDNS_CNAMEType {            
+            $destRec.CreateInstanceFromPropertyData($destServer, $dnsZone, $ownerName, $recordClass, $ttl, $recordData)            
+        }            
+        MicrosoftDNS_MXType {            
+            $preference   = $srcRec.Preference            
+            $mailExchange = $srcRec.MailExchange            
+            $destRec.CreateInstanceFromPropertyData($destServer, $dnsZone,$ownerName, $recordClass, $ttl, $preference, $mailExchange)            
+        }            
+        MicrosoftDNS_SRVType { 
+            
+            $priority   = $srcRec.Priority            
+            $weight     = $srcRec.Weight            
+            $port       = $srcRec.Port
+            $SrvDomainName = $srcRec.SRVDomainName           
+            $destRec.CreateInstanceFromPropertyData($destServer, $dnsZone, $ownerName, $recordClass, $ttl, $priority, $weight, $port,$SrvDomainName)            
+        }            
+        MicrosoftDNS_PTRType {            
+            $PTRDomainName   = $srcRec.PTRDomainName            
+            $destRec.CreateInstanceFromPropertyData($destServer, $dnsZone,$ownerName, $recordClass, $ttl, $PTRDomainName)            
+        }            
+    }            
+}            
--- get-DNSEntries.ps1
+++ get-DNSEntries.ps1
@@ -1,11 +1,11 @@
-﻿<#  
+﻿<#
 .SYNOPSIS  
-  This script collects DNS entries from a specifcied DNS zone on a specified server and then outputs them into csv file
+  The script collects DNS entries from a specified zone on a specified DNS server into csv file in the same directory
 .DESCRIPTION
-  This script uses WMI class MicrosoftDNS_ResourceRecord to collect all records off Windows DNS server, except for NS and SOA types.
-  The script, then creates a csv file in the same directory, from which it runs to csv file. The csv file can be used to reinset these records back into DNS
-DISCLAIMER
-    This Sample Code is provided for the purpose of illustration only and is not intended to be used in a production environment.
+  Script collect DNS entries other than of SOA and NS type and places them into csv file saved to the same directory from which script is ran.
+  The file name format is <dns zone name>-<current date and time>.csv. Warning: this script uses WMI and might cause perfomance problems on servers
+  with large number of DNS records and older hardware.
+  DISCLAIMER
     THIS SAMPLE CODE AND ANY RELATED INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED,
     INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.  
     We grant You a nonexclusive, royalty-free right to use and modify the Sample Code and to reproduce and distribute the object
@@ -13,22 +13,17 @@ DISCLAIMER
     product in which the Sample Code is embedded; (ii) to include a valid copyright notice on Your software product in which the
     Sample Code is embedded; and (iii) to indemnify, hold harmless, and defend Us and Our suppliers from and against any claims
     or lawsuits, including attorneys’ fees, that arise or result from the use or distribution of the Sample Code.
-    Please note: None of the conditions outlined in the disclaimer above will supersede the terms and conditions contained
-    within the Premier Customer Services Description.
-    
-.PARAMETER DNSZone
- Name of the DNS zone like contoso.com
-.PARAMETER DNSServer
-Name of the DNS server that hosts the zone
-.EXAMPLE
-   Run script from command line
-   ./get-DNSEntries.ps1 -DNSZome contoso.com -DNSserver myserver 
-
+    Please note: None of the conditions outlined in the disclaimer above will supersede the terms and conditions contained within
+    the Premier Customer Services Description.
+ .PARAMETER dnsZone
+  Name of the DNS zone from where records are collected
+ .PARAMETER DNSServer
+  Name of the DNS server where the DNS zone is hosted. Can use "." for local server
   
-    
+  .EXAMPLE
+  ./get-DNSEntries.ps1 -dnsZone contosoad.com -DNSServer dns1.contoso.com
+   
 #>
-
-
 
 
 [CmdletBinding()]
@@ -47,6 +42,9 @@ $DNSList = Get-WMIObject -ComputerName $DNSserver  -Namespace 'root\MicrosoftDNS
 
 ForEach($srcRec in $DnsList)
 {
+         
+     
+    
     $item = New-Object psobject -Property @{
     "class"         = $srcRec.__CLASS          # A, CNAME, PTR, etc.            
     "ownerName"     = $srcRec.OwnerName        # Name column in DNS GUI, FQDN            
@@ -54,12 +52,18 @@ ForEach($srcRec in $DnsList)
     "domainName"    = $srcRec.DomainName       # Zone FQDN            
     "ttl"           = $srcRec.TTL              # TTL            
     "recordClass"   = $srcRec.RecordClass      # Usually 1 (IN)            
-    "recordData"    = $srcRec.RecordData       # Data column in DNS GUI, value            
+    "recordData"    = $srcRec.RecordData       # Data column in DNS GUI, value
+    "priority"      = $srcRec.Priority         # Priority for SRV records
+    "weight"        = $srcRec.Weight           # Weight for SRV records
+    "port"          = $srcRec.Port             # Port for SRV Records
+    "PTRDomainName"   = $srcRec.PTRDomainName  # PTRDomainName
+    "SrvDomainName" = $srcRec.SrvDomainName
 
     }
+   
 
     $report += $item
 
 }
 $timeStamp = get-date -UFormat %Y%m%d-%I-%M-%S%p
-$report | Export-Csv "./$($dnsZone)-$($timeStamp).csv" -Force -NoTypeInformation
+$report | select class,ownerName,containerName,domainName,ttl,recordClass,recordData,SrvDomainName,priority,weight,port,PTRDomainName  | Export-Csv "./$($dnsZone)-$($timeStamp).csv" -Force -NoTypeInformation
